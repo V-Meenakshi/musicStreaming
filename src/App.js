3a9c5bc53046e2a854dd5caebdb214faa81232c2
SriKavya3753 <msrikavya2004@gmail.com>
2024-09-09 22:26:50 +0530

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createContext, useState } from "react";
 import Home from "./components/Home";
 import ProfilePage from "./components/ProfilePage";
+// import Login from "./components/auth/login";
+// import { setClientToken } from "./components/spotify";
 
 export const GlobalContext = createContext();
 function App() {
     const [globalIsLogin, setGlobalIsLogin] = useState(false); 
+    //const [token,setToken] = useState("");
+    // useEffect(()=>{
+    //     const token = window.localStorage.getItem("token")
+    //     const hash = window.location.hash;
+    //     window.location.hash = ""
+    //     if(!token && hash) {
+    //         const _token = hash.split("&")[0].split("=")[1];
+    //         window.localStorage.setItem("token", _token);
+    //         setToken(_token);
+    //         setClientToken(_token);
+    //     } else {
+    //         setToken(token);
+    //         setClientToken(token);+
+    //     }    
+    // },[])
     return (
         <GlobalContext.Provider
         value={{globalIsLogin, setGlobalIsLogin}}>
@@ -18,6 +35,7 @@ function App() {
                 </Routes>
             </BrowserRouter>
         </GlobalContext.Provider>
+        //<Login/>
     );
 }
 export default App;
---
+++ src/components/auth/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { loginEndpoint } from "../spotify";
+
+export default function Login(){
+    return (
+        <div className="login-page">
+            <a href={loginEndpoint}>
+                <div className="login-btn">LOG IN</div>
+            </a>
+        </div>
+    );
+}
